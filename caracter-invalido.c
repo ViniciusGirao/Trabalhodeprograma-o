108b77913f8bfdb2ba9ce22917438403b6a207c1
Vinícius Girão <viniciusdasilvagirao@gmail.com>
2024-09-30 20:32:33 -0300

    trabalho1.5

---
+++ caracter-invalido.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <math.h>
+#ifndef REMOVE_H
+#define REMOVE_H
+
+int valor_da_funcao(int x, int b) {
+    double resultado = (double)x / b;
+    return (int)round(resultado); // Vai arredondar e retornar //
+}
+
+#endif /* REMOVE_H */
+
+
+
+
+
+
+Carolina
+
+
+
+
+
+
+
+emanoel
+
+
+
+
+
+victor
